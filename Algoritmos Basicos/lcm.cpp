290de20856781b8a980eaa742032640ed8a71b17
usuario utp <usuario@HPdebian>
2017-03-30 19:15:43 -0500

    Máximo común divisor y Mínimo común múltiplo

---
+++ Algoritmos Basicos/lcm.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+ int lcm (int a, int b){
+    return (a / gcd(a, b)) * b;
+}
+
+int main(){
+  //Ejemplo:
+  cout << lcm(6, 10) << endl;
+  return 0;
+}
